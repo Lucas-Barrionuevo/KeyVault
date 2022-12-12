043a0bb989475b3e226c32e9c280e6f1f4746448
Agustin Aguilera <55515042+Aguilera-Agustin@users.noreply.github.com>
2022-12-12 08:56:51 -0300

    feat: login ui

--- packages/mobile/key_vault/lib/screens/login_screen.dart
+++ packages/mobile/key_vault/lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:key_vault/ui/input_decorations.dart';
 
@@ -9,17 +10,64 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
         body: SafeArea(
       child: Column(
-        children: const [
-          _Logo(),
-          _Title(),
-          _Subtitle(),
-          SizedBox(
+        children: [
+          const _Logo(),
+          const _Title(),
+          const _Subtitle(),
+          const SizedBox(
             height: 40,
           ),
-          _LoginForm()
+          const _LoginForm(),
+          const _ForgotMyPasswordButton(),
+          Expanded(
+            child: Container(),
+          ),
+          const _RegisterButton()
         ],
       ),
     ));
+  }
+}
+
+class _RegisterButton extends StatelessWidget {
+  const _RegisterButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+          style: const TextStyle(color: Colors.black54, fontSize: 17),
+          children: [
+            const TextSpan(
+              text: "¿No tienes una cuenta?",
+            ),
+            const WidgetSpan(child: SizedBox(width: 5)),
+            TextSpan(
+              recognizer: TapGestureRecognizer()
+                ..onTap = () => print('Tap Here onTap'),
+              text: "Registrate",
+              style: const TextStyle(color: Color(0xffF0C029)),
+            )
+          ]),
+    );
+  }
+}
+
+class _ForgotMyPasswordButton extends StatelessWidget {
+  const _ForgotMyPasswordButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: () {},
+        child: const Text(
+          "Olvidé mi contraseña",
+          style: TextStyle(color: Color(0xffF0C029)),
+        ));
   }
 }
 
@@ -68,6 +116,24 @@ class _LoginFormState extends State<_LoginForm> {
                       Icons.remove_red_eye_outlined,
                       color: Colors.black54,
                     ))),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          MaterialButton(
+            onPressed: () {},
+            color: const Color(0xffF0C029),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            child: Container(
+              width: double.infinity,
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: const Text(
+                "Iniciar sesión",
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ),
           ),
         ],
       )),
