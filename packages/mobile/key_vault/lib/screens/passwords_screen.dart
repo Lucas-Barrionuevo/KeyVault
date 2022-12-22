691c7e51bbb9afb3173dc6b1a0abad7342eecfc0
Agustin Aguilera <agustin.aguilera424@gmail.com>
2022-12-22 09:51:19 -0300

    feat: password

--- packages/mobile/key_vault/lib/screens/passwords_screen.dart
+++ packages/mobile/key_vault/lib/screens/passwords_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:key_vault/ui/input_decorations.dart';
 
 class PasswordsScreen extends StatelessWidget {
   const PasswordsScreen({Key? key}) : super(key: key);
@@ -6,6 +7,7 @@ class PasswordsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
           title: const Text("Password"),
           backgroundColor: Colors.white,
@@ -14,8 +16,28 @@ class PasswordsScreen extends StatelessWidget {
           toolbarHeight: 70,
           titleTextStyle: const TextStyle(
               color: Colors.black, fontSize: 25, fontWeight: FontWeight.bold)),
-      body: const Center(
-        child: Text('PasswordsScreen'),
+      body: Column(
+        children: const [
+          _SearchBar(),
+        ],
+      ),
+    );
+  }
+}
+
+class _SearchBar extends StatelessWidget {
+  const _SearchBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 50),
+      child: TextFormField(
+        style: const TextStyle(
+            color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18),
+        decoration: InputDecorations.searchDecoration(),
       ),
     );
   }
--- packages/mobile/key_vault/lib/ui/input_decorations.dart
+++ packages/mobile/key_vault/lib/ui/input_decorations.dart
@@ -5,4 +5,24 @@ class InputDecorations {
       {required String labelText, Widget? suffixIcon}) {
     return InputDecoration(labelText: labelText, suffixIcon: suffixIcon);
   }
+
+  static InputDecoration searchDecoration() {
+    return InputDecoration(
+        enabledBorder: OutlineInputBorder(
+          // width: 0.0 produces a thin "hairline" border
+          borderSide: const BorderSide(color: Color(0xffF6F8F9), width: 0.0),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        filled: true,
+        prefixIcon: const Icon(
+          Icons.search,
+          color: Color(0xffA6AFB3),
+        ),
+        hintStyle: const TextStyle(
+            color: Color(0xffA6AFB3),
+            fontWeight: FontWeight.bold,
+            fontSize: 18),
+        hintText: "Search",
+        fillColor: const Color(0xffF6F8F9));
+  }
 }
