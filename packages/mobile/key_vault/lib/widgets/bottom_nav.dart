23fbb65baacfa0e01730efab20934363d27ede9c
Agustin Aguilera <agustin.aguilera424@gmail.com>
2022-12-22 11:07:56 -0300

    feat: bottom nav with provider

--- packages/mobile/key_vault/lib/widgets/bottom_nav.dart
+++ packages/mobile/key_vault/lib/widgets/bottom_nav.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:key_vault/providers/providers.dart';
 import 'package:key_vault/theme/app_theme.dart';
+import 'package:provider/provider.dart';
 
 class BottomNav extends StatelessWidget {
   const BottomNav({
@@ -8,11 +10,15 @@ class BottomNav extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final bottomNavProvider = Provider.of<BottomNavProvider>(context);
+    final currentIndex = bottomNavProvider.selectedMenuOpt;
     return BottomNavigationBar(
         backgroundColor: Colors.white,
         showSelectedLabels: false,
+        currentIndex: currentIndex,
         showUnselectedLabels: false,
         selectedItemColor: AppTheme.primary,
+        onTap: (value) => bottomNavProvider.selectedMenuOpt = value,
         iconSize: 28,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
