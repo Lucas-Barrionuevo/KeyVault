b316587745b2d18681a9a660b75117bb0ded2e05
Lucas-Barrionuevo <lbarrionuevo2000@gmail.com>
2022-12-22 14:57:01 -0300

    update

--- packages/backend/src/main/java/com/keyVault/app/security/TokenUtils.java
+++ packages/backend/src/main/java/com/keyVault/app/security/TokenUtils.java
@@ -1,5 +1,28 @@
 package com.keyVault.app.security;
 
-public class TokenUtils {
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.data.repository.query.QueryLookupStrategy.Key;
+
+import io.jsonwebtoken.Jwts;
+
+public class TokenUtils {
+	/*private final static String ACCESS_TOKEN_SECRET = "";
+	private final static long ACCESS_TOKEN_VALIDITY_SECONDS = 2_592_000L;
+	
+	public static String createToken(String nombre, String email) {
+		long expirationTime = ACCESS_TOKEN_VALIDITY_SECONDS * 1000;
+		Date expirationDate = new Date(System.currentTimeMillis()* expirationTime);
+		
+		Map<String, Object> extra = new HashMap<>();
+		extra.put("nombre", nombre);
+		
+		return Jwts.builder()
+				.setSubject(email)
+				.setExpiration(expirationDate)
+				.addClaims(extra)
+				.signWith(Keys.hmacShakeyFor(ACCESS_TOKEN_SECRET.getBytes()))
+	}*/
 }
